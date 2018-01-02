5b657ea71dd95fc9b04cbf6e75159a8232c5f47f
Pastor Monjas <mpmonjas@indra.es>
2018-01-02 18:11:17 +0100

    first commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
-import { relative } from 'path';
 import { DataService } from '../data.service';
 
 @Component({
